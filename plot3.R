1bc45f52c1b8af80c07a59989e12ef4590399520
fborsato <fabianoborsato@hotmail.com>
2015-03-03 20:10:48 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,25 +1,26 @@
-## Fetches full dataset
-base_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ';', na.strings = "?", check.names = FALSE, stringsAsFactors = FALSE, comment.char="", quote='\"')
-base_data$Date <- as.Date(base_data$Date, format="%d/%m/%Y")
+## Getting full dataset
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
-## Subsets the data
-data <- subset(base_data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(base_data)
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
-## Converts dates
-date_time <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(date_time)
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
 ## Plot 3
 with(data, {
-    plot(Sub_metering_1~Datetime, type = "l",
-         ylab = "Global Active Power (kilowatts)", xlab = "")
-    lines(Sub_metering_2~Datetime, col = 'Red')
-    lines(Sub_metering_3~Datetime, col = 'Blue')
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
 })
-legend("topright", col=c("black", "red", "blue"), lty = 1, lwd = 2, 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-## Saves data to file
-dev.copy(png, file = "plot3.png", height = 480, width = 480)
+## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
